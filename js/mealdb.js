871eb563fba2a69678c57db6aeed351a824e0dfa
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-03-04 01:19:38 +0600

    some more functionality added

--- js/mealdb.js
+++ js/mealdb.js
@@ -1,32 +1,66 @@
 const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
+
+    // clear data from extra information div
+    const mealDetails = document.getElementById('meal-details');
+    mealDetails.textContent = '';
+
     // clear data
     searchField.value = '';
 
-    // load data
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    if (searchText == '') {
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchResult(data.meals))
+        const noMeal = document.getElementById('no-meal');
+        noMeal.textContent = '';
+
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <h2>Empty Search field..</h2>
+        `
+        const addSearchInput = document.getElementById('empty-search');
+        addSearchInput.innerText = '';
+
+        addSearchInput.appendChild(div);
+
+        const searchResult = document.getElementById('search-result');
+        searchResult.textContent = '';
+    } else {
+        const addSearchInput = document.getElementById('empty-search');
+        addSearchInput.innerText = '';
+        // load data
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchResult(data.meals))
+    }
+
 }
 
 const displaySearchResult = meals => {
     // console.log(meals);
-    const searchResult = document.getElementById('search-result');
-    searchResult.textContent = '';
+
     if (meals == null) {
         const noMeal = document.getElementById('no-meal');
+        noMeal.innerText = '';
+
+        const searchResult = document.getElementById('search-result');
+        searchResult.textContent = '';
+
         const div = document.createElement('div');
         div.innerHTML = `
             <h2>No Meal Found..</h2>
         `;
         noMeal.appendChild(div);
     } else {
+        const searchResult = document.getElementById('search-result');
+        searchResult.textContent = '';
+
         meals.forEach(meal => {
             const noMeal = document.getElementById('no-meal');
             noMeal.textContent = '';
+
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
@@ -38,12 +72,10 @@ const displaySearchResult = meals => {
                     </div>
                 </div>
                 `;
+
             searchResult.appendChild(div);
         })
     }
-
-
-
 
 }
 
@@ -62,6 +94,7 @@ const displayMealDetail = (meal) => {
     // console.log(meal);
     const mealDetails = document.getElementById('meal-details');
     mealDetails.textContent = '';
+
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
